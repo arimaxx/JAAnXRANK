93bcb14d3827152bffa8dbed598b7ffc57f71325
arimaxx <155280132+arimaxx@users.noreply.github.com>
2024-03-17 12:53:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def top_members_with_image(message, text, photo_url):
         )
     )
 
-@app.on_message(filters.command("ranking, rank, rankings"))
+@app.on_message(filters.command(["ranking", "rank", "rankings"]))
 async def send_rankings_with_image(_, message):
     image_url = random.choice(IMAGE_URLS)
     text = "𝗧𝗵𝗶𝘀 𝗶𝘀 𝗧𝗦 𝗥𝗮𝗻𝗸𝗶𝗻𝗴 𝗕𝗼𝘁 \n 𝗰𝗼𝘂𝗻𝘁 𝘁𝗵𝗲 𝗰𝗵𝗮𝘁 𝗮𝗰𝘁𝗶𝘃𝗶𝘁𝘆 𝗼𝗳 𝘂𝘀𝗲𝗿𝘀 𝗶𝗻 𝘁𝗵𝗶𝘀 𝗴𝗿𝗼𝘂𝗽 \n 𝗬𝗼𝘂𝗿 𝗿𝗮𝗻𝗸𝗶𝗻𝗴 𝘁𝗲𝘅𝘁 𝗴𝗼𝗲𝘀 𝗵𝗲𝗿𝗲..."
